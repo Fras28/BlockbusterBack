3d76a55e4ec32054a5e4384c5cb200df4271c87a
Franco Selvarolo <fselvarolo28@gmail.com>
2022-09-23 13:53:26 -0300

    para merge main

--- dist/controller/BDcontroller.js
+++ dist/controller/BDcontroller.js
@@ -13,11 +13,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fullDbMovies = exports.getAllMovies = void 0;
-const blockbuster_model_1 = __importDefault(require("../db/models/blockbuster.model"));
+const Blockbuster_model_1 = __importDefault(require("../db/models/Blockbuster.model"));
 const blockbuster_service_1 = require("../services/blockbuster.service");
 const infoSec_1 = require("../infoSec");
 const filmsName = infoSec_1.MoviesArr;
-const blockbusterService = new blockbuster_service_1.BlockbusterService(new blockbuster_model_1.default());
+const blockbusterService = new blockbuster_service_1.BlockbusterService(new Blockbuster_model_1.default());
 const getAllMovies = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const result = yield blockbusterService.getAll();
