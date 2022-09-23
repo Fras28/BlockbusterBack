12340ee8acee5cdd428551fbd1186e79672431e4
Franco Selvarolo <fselvarolo28@gmail.com>
2022-09-23 13:53:49 -0300

    comit

--- dist/routes/blockbuster.routes.js
+++ dist/routes/blockbuster.routes.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const blockbuster_controller_1 = require("../controller/blockbuster.controller");
 const users_controller_1 = require("../controller/users.controller");
-const admin_controller_1 = require("../controller/admin.controller");
+// import { editeMovie } from "../controller/admin.controller";
 const router = (0, express_1.Router)();
 //router.get("/home", getAllMovies);
 router.get("/", blockbuster_controller_1.fullDbMovies);
 router.post('/addM', blockbuster_controller_1.addMovie);
 router.get('/detail/:id', blockbuster_controller_1.getMovieId);
 router.post('/newU', users_controller_1.addUser);
-router.patch('/edit/:id', admin_controller_1.editeMovie);
+// router.patch('/movies/edit/:id', editeMovie)
 exports.default = router;
