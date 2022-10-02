58c5ea6ea8943dfefca7f0139a3ced3c446a1ae1
Franco Selvarolo <fselvarolo28@gmail.com>
2022-10-02 16:32:19 -0300

    false coments/users

--- src/db/init.ts
+++ src/db/init.ts
@@ -5,8 +5,8 @@ import user from "./models/users.model";
 const dbInit = () =>
 Promise.all([
   blockbuster.sync({ alter: true }),
-  comments.sync({ alter: true }),
-  user.sync({ alter: true }),
+  comments.sync({ alter: false }),
+  user.sync({ alter: false }),
 ])
 
 export default dbInit;
