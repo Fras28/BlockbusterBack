f593d3da32f94bfeaaaa064a275d676df9c8ba8e
Franco Selvarolo <fselvarolo28@gmail.com>
2022-10-03 13:27:11 -0300

    node moduls

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ dotenv.config();
 dbInit();
 
 const app: Application = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
